32d681cd91cf64d602ca91d639e1496b4fca13a5
nylesgeiger <nylesgeiger@10.36.227.4>
2020-10-20 07:09:27 -0400

    small changes and format correcting
    Signed-off-by: nylesgeiger <nylesgeiger@10.36.227.4>

--- Database.java
+++ Database.java
@@ -1,42 +1,43 @@
 
 public class Database {
 
-	// possibly have methods return strings announcing that the action will be performed
-	
-	public void createRenter(String username, String password, String email, 
-			String phoneNumber, String name, String bio, String uscID) {
-	
-		Renter  newRenter = new Renter (username, password, email, uscID, phoneNumber, name, bio);
+	// possibly have methods return strings announcing that the action will be
+	// performed
+
+	public void createRenter(String username, String password, String email, String phoneNumber, String name,
+			String bio, String uscID) {
+
+		Renter newRenter = new Renter(username, password, email, uscID, phoneNumber, name, bio);
 		// json needed
 	}
-	
-	public void createSeller(String username, String password, String email, String userID, String phoneNumber, String name,
-			String bio) {
-		Seller newSeller = new Seller( username, password, email, userID, phoneNumber, name, bio);
-		//json needed
+
+	public void createSeller(String username, String password, String email, String userID, String phoneNumber,
+			String name, String bio) {
+		Seller newSeller = new Seller(username, password, email, userID, phoneNumber, name, bio);
+		// json needed
 	}
-	
-	public void createRenterSeller(String username, String password, String email, 
-			String uscID, String phoneNumber, String name, String bio) {
-		
-		Renter  newRenterSeller = new Renter (username, password, email, uscID, phoneNumber, name, bio);
+
+	public void createRenterSeller(String username, String password, String email, String uscID, String phoneNumber,
+			String name, String bio) {
+
+		Renter newRenterSeller = new Renter(username, password, email, uscID, phoneNumber, name, bio);
 		newRenterSeller.makeSeller();
-		// call isSeller method
+
 	}
-	
+
 	public void removeUser(int UserID) {
 		// json
 	}
-	
+
 	public void saveProperty(Property property) {
-		//json
+		// json
 	}
-	
+
 	public void deleteProperty(int prpoertyID) {
-		//json
+		// json
 	}
-	
+
 	public boolean verifyUserLogin(String username, String password) {
-		return false; //implement logic
+		return false; // implement logic
 	}
 }

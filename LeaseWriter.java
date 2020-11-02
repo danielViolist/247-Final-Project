b41fe5a063f83b02bfb31307b51c540d3f416dab
uscNyles <70451912+uscNyles@users.noreply.github.com>
2020-11-01 23:02:28 -0500

    Progression on Lease writer

--- LeaseWriter.java
+++ LeaseWriter.java
@@ -1,48 +1,75 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class LeaseWriter {
 
 	private PrintWriter output;
-	private Scanner input;/*Seller seller, Renter renter, Date startDate, Date endDate*/
+	private Scanner inputByLine;
+	private Scanner inputByWord;
 
-	public void SignLease() throws FileNotFoundException {
-		input = new Scanner(new File("LeaseAgreement.txt"));
+	public void SignLease(/*String ownerName, ArrayList<String> tenantNames, Date startDate, Date endDate*/) throws FileNotFoundException {
+		inputByLine = new Scanner(new File("LeaseAgreement.txt"));
 		output = new PrintWriter("SignedLease.txt");
 
-		while (input.hasNext()) {
+		while (inputByLine.hasNextLine()) {
 
-			String line = input.next();
+			/**
+			 * Reads in line of the file.
+			 * The purpose of the first scanner is to start a new line 
+			 * when the read in file does so
+			 */
+			String fileLine = inputByLine.nextLine();
+			
+			/**
+			 * The purpose of the scanner is to read in the line
+			 * given by the first scanner to edit each word 
+			 * individually
+			 */
+			inputByWord = new Scanner(fileLine);
+			
+			while (inputByWord.hasNext()) {
+
+				String fileWord = inputByWord.next();
+
+				if (fileWord.equalsIgnoreCase("<DATE>"))
+					fileWord = "<Must be replaced>";
+				if (fileWord.equalsIgnoreCase("<LANDLOARD>"))
+					fileWord = "<Must be replaced>";
+				if (fileWord.equalsIgnoreCase("<NUM_BED>"))
+					fileWord = "<Must be replaced>";
+				if (fileWord.equalsIgnoreCase("<NUM_BATH>"))
+					fileWord = "<Must be replaced>";
+				if (fileWord.equalsIgnoreCase("<PROPERTY_ADDRESS>"))
+					fileWord = "<Must be replaced>";
+				if (fileWord.equalsIgnoreCase("<ZIP>"))
+					fileWord = "<Must be replaced>";
+				if (fileWord.equalsIgnoreCase("<START DATE>"))
+					fileWord = "<Must be replaced>";
+				if (fileWord.equalsIgnoreCase("<END DATE>"))
+					fileWord = "<Must be replaced>";
+				if (fileWord.equalsIgnoreCase("<RENT>"))
+					fileWord = "<Must be replaced>";
+				if (fileWord.equalsIgnoreCase("<PAYMENT ADDRESS>"))
+					fileWord = "<Must be replaced>";
+				if (fileWord.equalsIgnoreCase("<DAMAGE COST>"))
+					fileWord = "<Must be replaced>";
+				if (fileWord.equalsIgnoreCase("<TENANT(s)>"))
+					fileWord = "Must be replaced>";
+				
+
+				fileWord += " ";
+
+				
+			}
+			output.print(fileLine);
+			inputByWord.close();
 			
 			
-			if (line.equalsIgnoreCase("<SignDATE>")) {
-				line = "<Must be replaced>";}
-			if (line.equalsIgnoreCase("<LANDLOARD>")) {
-				line = "<Must be replaced>";}
-			if (line.equalsIgnoreCase("<NUM_BED>")) {
-				line = "<Must be replaced>";}
-			if (line.equalsIgnoreCase("<NUM_BATH>")) {
-				line = "<Must be replaced>";}
-			if (line.equalsIgnoreCase("<PROPERTY_ADDRESS>")) {
-				line = "<Must be replaced>";}
-			/*if (line.equalsIgnoreCase("<ZIP>"));{
-				line = "<Must be replaced>";}*/ //makes the entire document <Must be replaced>
-			if (line.equalsIgnoreCase( "<START DATE>")) {
-				line = "<Must be replaced>";}
-			if (line.equalsIgnoreCase("<END DATE>")) {
-				line = "<Must be replaced>";}
-			if (line.equalsIgnoreCase( "<RENT>")) {
-				line = "<Must be replaced>";}
-			if (line.equalsIgnoreCase( "<PAYMENT ADDRESS>")) {
-				line = "<Must be replaced>";}
-			if (line.equalsIgnoreCase("<DAMAGE COST>")) {
-				line = "<Must be replaced>";}
-		
-				output.println(line);
 		}
-		input.close();
-		output.close();
+		inputByLine.close();
+			output.close();
 	}
 }
